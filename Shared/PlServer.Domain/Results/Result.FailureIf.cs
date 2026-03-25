b98a7e761ec7b2db7d61fe073ed03474b8d10d49
InvariantTime <diod.vt@gmail.com>
2026-03-25 23:52:48 +0300

    rename FailureIf to Check

--- Shared/PlServer.Domain/Results/Result.FailureIf.cs
+++ Shared/PlServer.Domain/Results/Result.FailureIf.cs
@@ -3,7 +3,7 @@ namespace PlServer.Domain.Results;
 
 public readonly partial struct Result
 {
-    public static Result FailureIf(bool condition, ErrorTypes error)
+    public static Result Check(bool condition, ErrorTypes error)
     {
         if (condition == true)
             return Success();
@@ -11,7 +11,7 @@ public readonly partial struct Result
         return Failure(error);
     }
 
-    public static Result FailureIf(bool condition, ErrorTypes error, string description)
+    public static Result Check(bool condition, ErrorTypes error, string description)
     {
         if (condition == true)
             return Success();
@@ -19,7 +19,7 @@ public readonly partial struct Result
         return Failure(error, description);
     }
 
-    public static Result FailureIf(bool condition, Error<ErrorTypes> error)
+    public static Result Check(bool condition, Error<ErrorTypes> error)
     {
         if (condition == true)
             return Success();
@@ -27,7 +27,7 @@ public readonly partial struct Result
         return Failure(error);
     }
 
-    public static UnitResult<TError> FailureIf<TError>(bool condition, TError error) where TError : struct
+    public static UnitResult<TError> Check<TError>(bool condition, TError error) where TError : struct
     {
         if (condition == true)
             return Success<TError>();
@@ -35,7 +35,7 @@ public readonly partial struct Result
         return Failure(error);
     }
 
-    public static UnitResult<TError> FailureIf<TError>(bool condition, TError error, string description) where TError : struct
+    public static UnitResult<TError> Check<TError>(bool condition, TError error, string description) where TError : struct
     {
         if (condition == true)
             return Success<TError>();
@@ -43,7 +43,7 @@ public readonly partial struct Result
         return Failure(error, description);
     }
 
-    public static UnitResult<TError> FailureIf<TError>(bool condition, Error<TError> error) where TError : struct
+    public static UnitResult<TError> Check<TError>(bool condition, Error<TError> error) where TError : struct
     {
         if (condition == true)
             return Success<TError>();
