8ed0caca5a8ecd0c3c2567cff1edfbb5a6bfbeee
InvariantTime <diod.vt@gmail.com>
2026-04-13 22:45:23 +0300

    start to write pipeline builder

---
+++ Shared/PlServer.Domain.NodeGraph/PipelineBuilder.cs
@@ -0,0 +1,14 @@
+﻿using PlServer.Domain.Results;
+
+namespace PlServer.Domain.Nodes;
+
+internal static class PipelineBuilder //TODO: non static ?
+{
+    public static CommandActivator Build(IEnumerable<INodeGraphPolicy> policies, Type commandType)
+    {
+        return (command, context) =>
+        {
+            return Result.Success<NodeErrors>();
+        };
+    }
+}
